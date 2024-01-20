7da4e428de16efb5aaaa479a38981a4b8ad1669d
byiringiroaimefils <aimefils173@gmail.com>
2024-01-20 11:08:54 +0200

    v2

--- top.js
+++
@@ -1,8 +0,0 @@
-let button = document.getElementById("btn-top")
-console.log(button);
-button.addEventListener("click",function(){
-window.scrollTo({top:0, behavior:"smooth"})
-console.log("clicked");
-} 
-
-)
